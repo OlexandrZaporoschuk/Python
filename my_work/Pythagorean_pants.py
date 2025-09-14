f49a6d17171b69bbfa695a9a3c7f5d9b125b6ba3
Alex <olexandr.zaporoschuk@gmail.com>
2025-09-14 16:02:22 +0300

    Перевірка  для завдання 1

--- my_work/Pythagorean_pants.py
+++ my_work/Pythagorean_pants.py
@@ -1,12 +1,19 @@
-def Pythagorean_pants(numbers):
-    """Функція Pythagorean_pants приймає три параметри у вигляді цілих додатніх чисел"""
-    numbers_ = sorted(numbers)
+def Pythagorean_pants(numbers = 0):
+    """Функція Pythagorean_pants приймає 1 параметр у вигляді списку з 3-ма цілими додатніми числами"""
+    try:
+        numbers_ = sorted(numbers)
+    except TypeError :
+        if numbers == 0 :
+            return "Ви забули ввести параметр"
+        print("Перевірте, будь ласка, тип даних параметра!")
+        return "Ви ввели", numbers, type(numbers)
+   
     if numbers_[0] ** 2 + numbers_[1] ** 2 == numbers_[2] ** 2 :
         return True
     else:
         return False
-print(Pythagorean_pants([5, 3, 4]))
+print(Pythagorean_pants(["ііі", 3, 4]))
 print(Pythagorean_pants([6, 8, 10]))   
 print(Pythagorean_pants([100, 3, 65]))   
-
+  
 __version__ = '1.0'
