5c263c04f2483e2a43df65778ea4e198d88df698
Alex <olexandr.zaporoschuk@gmail.com>
2025-09-16 08:57:53 +0300

    Перевірка помилок в завданні 2

--- my_work/Plants_vs_Zombies.py
+++ my_work/Plants_vs_Zombies.py
@@ -1,6 +1,22 @@
-def plants_vs_zombies(list1, list2):
+def plants_vs_zombies(list1 = 0, list2 = 0):
     # Створюю документацію
     """Функція plants_vs_zombies приймає 2 параметри у вигляді списків з натуральними числами """
+    
+    # Перевірка помилок
+    if list1 == 0 or list2 == 0:
+        return "Ви забули ввести параметр"
+    
+    if  not(isinstance(list1, list) and isinstance(list2, list)):
+        return "Перевірте, будь ласка, тип даних параметрів!"
+    
+    for i in list1:
+        if not isinstance(i, int):
+            return "Перевірте тип даних в членах списків"
+    for i in list2:
+        if not isinstance(i, int):
+            return "Перевірте тип даних в членах списків"
+    
+    
 
     # Створюю змінні для роботи функції
     plants = 0
@@ -38,9 +54,9 @@ def plants_vs_zombies(list1, list2):
 
 # Перевірка
 
-print(plants_vs_zombies([ 1, 3, 5, 7 ], [ 2, 4, 6, 8 ]))
-print(plants_vs_zombies([ 1, 3, 5, 7 ], [ 2, 4 ]))
-print(plants_vs_zombies([ 1, 3, 5, 7 ], [ 2, 4, 0, 8 ]))
+print(plants_vs_zombies([ 1, 3, 5, 7 ]))
+print(plants_vs_zombies([ 1, 3, 5, 7 ], "qq"))
+print(plants_vs_zombies([ 1, 3, 5, 7 ], [ 2, 4, "qq", 8 ]))
 print(plants_vs_zombies([ 2, 1, 1, 1 ], [ 1, 2, 1, 1 ]))
 
 #zombies=[ 1, 3, 5, 7 ] plants=[ 2, 4, 6, 8 ] -> True
