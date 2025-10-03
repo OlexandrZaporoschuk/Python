1f6b723e855ae35aab7a4435a5f5ae02104d9cf6
Alex <olexandr.zaporoschuk@gmail.com>
2025-10-03 12:34:41 +0300

    Перевірка помилок для завдання 3

--- my_work/employee_workday_schedule_generator.py
+++ my_work/employee_workday_schedule_generator.py
@@ -1,10 +1,14 @@
 import datetime
+import inspect
 
 def employee_WSG(days: int, work_days: int, rest_days: int, start_date: datetime.datetime):
     """ Функція employee_WSG  - функція, яка генерує розклад робочих днів працівника, вона приймає 
     кількість днів, на які потрібно скласти розклад, кількість днів роботи, кількість днів відпочинку
       та дату початку розкладу. """
-    
+    # Робота над помилками
+    if not(isinstance(days, int) and isinstance(work_days, int) and isinstance(rest_days, int) and inspect.isclass(datetime.datetime)):
+        return "Перевірьте, будь ласка типи параметрів, що ви ввели!"
+
     schedule = []
     current_date = start_date
     total_days_added = 0
@@ -27,4 +31,6 @@ def employee_WSG(days: int, work_days: int, rest_days: int, start_date: datetime
 
 
 print(employee_WSG(5, 2, 1, datetime.datetime(2020, 1, 30)))
+print(employee_WSG("eee", 2, 1, datetime.datetime(2020, 1, 30)))
+print(employee_WSG.__doc__)
 #days: 5, work_days: 2, rest_days: 1, start_date: datetime(2020, 1, 30)
